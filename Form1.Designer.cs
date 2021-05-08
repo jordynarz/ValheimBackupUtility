e09392bf299a2e3cae7bd4da7575d05b137685c3
Jordynarz <Jordynarz@gmail.com>
2021-05-07 17:16:27 -0700

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace ValheimBackup
+{
+    partial class ValheimBackup
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ValheimBackup));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.BtnOpenDest = new System.Windows.Forms.Button();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.BtnBackup = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAppInfo = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.LblBrowse = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Papyrus", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(165, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(454, 64);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Valheim Backup Utility";
+            // 
+            // BtnOpenDest
+            // 
+            this.BtnOpenDest.Location = new System.Drawing.Point(346, 82);
+            this.BtnOpenDest.Name = "BtnOpenDest";
+            this.BtnOpenDest.Size = new System.Drawing.Size(126, 37);
+            this.BtnOpenDest.TabIndex = 3;
+            this.BtnOpenDest.Text = "Open Folder";
+            this.BtnOpenDest.UseVisualStyleBackColor = true;
+            this.BtnOpenDest.Click += new System.EventHandler(this.BtnOpenDest_Click);
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(478, 30);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(89, 37);
+            this.BtnBrowse.TabIndex = 1;
+            this.BtnBrowse.Text = "Browse...";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // BtnBackup
+            // 
+            this.BtnBackup.Location = new System.Drawing.Point(632, 309);
+            this.BtnBackup.Name = "BtnBackup";
+            this.BtnBackup.Size = new System.Drawing.Size(156, 89);
+            this.BtnBackup.TabIndex = 2;
+            this.BtnBackup.Text = "Backup";
+            this.BtnBackup.UseVisualStyleBackColor = true;
+            this.BtnBackup.Click += new System.EventHandler(this.BtnBackup_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAppInfo);
+            this.groupBox1.Controls.Add(this.BtnClear);
+            this.groupBox1.Controls.Add(this.LblBrowse);
+            this.groupBox1.Controls.Add(this.BtnBrowse);
+            this.groupBox1.Controls.Add(this.BtnOpenDest);
+            this.groupBox1.Location = new System.Drawing.Point(12, 296);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(591, 142);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Destination";
+            // 
+            // btnAppInfo
+            // 
+            this.btnAppInfo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnAppInfo.Location = new System.Drawing.Point(15, 82);
+            this.btnAppInfo.Name = "btnAppInfo";
+            this.btnAppInfo.Size = new System.Drawing.Size(108, 36);
+            this.btnAppInfo.TabIndex = 5;
+            this.btnAppInfo.Text = "App INFO";
+            this.btnAppInfo.UseVisualStyleBackColor = true;
+            this.btnAppInfo.Click += new System.EventHandler(this.btnAppInfo_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(478, 82);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(89, 37);
+            this.BtnClear.TabIndex = 4;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // LblBrowse
+            // 
+            this.LblBrowse.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LblBrowse.Location = new System.Drawing.Point(15, 30);
+            this.LblBrowse.Name = "LblBrowse";
+            this.LblBrowse.Size = new System.Drawing.Size(457, 37);
+            this.LblBrowse.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(632, 404);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(156, 34);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Value = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(48, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(710, 202);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ValheimBackup
+            // 
+            this.AcceptButton = this.BtnBackup;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BtnBackup);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ValheimBackup";
+            this.Text = "Valheim Backup Utility";
+            this.Load += new System.EventHandler(this.ValheimBackup_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button BtnOpenDest;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.Button BtnBackup;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label LblBrowse;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAppInfo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO.Compression;
+
+
+namespace ValheimBackup
+{
+    public partial class ValheimBackup : Form
+    {
+        public ValheimBackup()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+
+        /*  Create an advanced button that reveals controls to select folder of files to be copied.
+         *  Save Settings to remeber previous configuration 
+         * 
+         */
+        private void ValheimBackup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+                // Fetch the current user of the machine to use as the start path to the Valheim files. This grabs the current user profile.
+                string theUser = Convert.ToString(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+
+            // Combine the USER path to the path where Valheim files reside.
+                string valheimPath = Path.Combine(theUser, "AppData/LocalLow/IronGate/Valheim");
+
+            // Full path to the "worlds" folder to grab all the world files from.
+                string worldsPath = Path.GetFullPath(Path.Combine(valheimPath, "worlds"));
+
+            // Full path to the "characters" folder to grab all the character files from.
+                string charsPath = Path.GetFullPath(Path.Combine(valheimPath, "characters"));
+
+
+                // If the Valheim AppData folder does not exist Show message and "return" to stop the program from trying to execute.
+                if (!Directory.Exists(Path.GetFullPath(valheimPath)))
+                {
+                    MessageBox.Show("Cannot locate Valheim saves.\nPlease ensure Valheim is installed.\nOr run with Admin privileges.");
+                    return;
+                }
+
+                if (!Directory.Exists(worldsPath))
+                {
+                    MessageBox.Show("Cannot locate Valheim world saves.\nPlease ensure Valheim is installed.\nOr run with Admin privileges.");
+                    return;
+                }
+
+                if (!Directory.Exists(charsPath))
+                {
+                    MessageBox.Show("Cannot locate Valheim character saves.\nPlease ensure Valheim is installed.\nOr run with Admin privileges.");
+                    return;
+                }
+
+                // if the backup location label is empty display a message.
+                if (LblBrowse.Text == "")
+                {
+                    MessageBox.Show("You have not selected a backup destination. Please browse to desired backup location.");
+                }
+
+                // Do the Backup
+                else
+                {
+                    // Play audio?
+                    System.IO.Stream strm = Properties.Resources.vMusS;
+                    System.Media.SoundPlayer snd = new System.Media.SoundPlayer(strm);
+                    snd.Play();
+
+
+
+                    // set the progress bar to visible min level and max level as well as current value
+                    progressBar1.Visible = true;
+                    progressBar1.Minimum = 1;
+                    progressBar1.Maximum = 4;
+                    progressBar1.Value = 1;
+                    progressBar1.Step = 1;
+                    // step the progress bar to 2 of 4
+                    progressBar1.PerformStep();
+
+                    // create folder with current date.
+                    string dir = Path.GetFullPath(LblBrowse.Text +@"\" + DateTime.Now.ToString("MM-dd-yy - HH.mm").Insert(11, "h").Replace(".", ""));
+
+
+
+                    // backup the worlds
+                    string startPath = worldsPath;
+                    string zipPath = @".\worlds.zip";
+                    string extractPath = dir + @"\worlds";
+
+                    // if the zip file already exists. Delete it so the app can zip it up again.
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+                    // create a zip folder with the "worlds" folder contents
+                    ZipFile.CreateFromDirectory(startPath, zipPath);
+                    // unzip the zip to copy the content of the folder to a new directory
+                    ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+                    // clean up the zip files that were created by deleting them. 
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+                    // step the progress bar to 3 of 4
+                    progressBar1.PerformStep();
+
+
+                    // backup the characters
+                     startPath = charsPath;
+                     zipPath = @".\characters.zip";
+                     extractPath = dir + @"\characters";
+
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+                    // create a zip folder with the "characters" folder contents
+                    ZipFile.CreateFromDirectory(startPath, zipPath);
+
+                    // step the progress bar to 4 of 4
+                    progressBar1.PerformStep();
+
+                    // unzip the zip to copy the content of the folder to a new directory
+                    ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+
+
+                    // clean up the zip files that were created by deleting them. 
+                    // Might be good to save these as zipped in the first place.
+                    // Maybe have another button "Backup as ZIP" 
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+
+
+
+                    // The program has successfully finished display a Backup complete box.
+                    if (Directory.Exists(Path.GetFullPath(extractPath))
+                        &&  !File.Exists(zipPath)
+                        
+                        )
+                    {
+                        MessageBox.Show("Backup Complete");
+                        progressBar1.Visible = false;
+                    }
+                    
+                }
+                
+            }
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                MessageBox.Show(dirNotFound.Message);
+                
+            }
+            catch (Exception sys)
+            {
+                MessageBox.Show(sys.Message);
+            }
+
+        }
+
+
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Browse for a file or folder path to use for Backup location
+                FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    LblBrowse.Text = folderBrowserDialog1.SelectedPath;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+    }
+
+        private void BtnOpenDest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(LblBrowse.Text == "")
+                {
+                    MessageBox.Show("You have not selected a backup destination. Please browse to desired backup location.");
+                }
+                else
+                {
+                // Open the Selected Backup Location
+                System.Diagnostics.Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", LblBrowse.Text);
+                }
+
+ 
+            }
+            catch (Win32Exception win32Exception)
+            {
+                MessageBox.Show(win32Exception.Message);
+            }
+
+
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            // Clear the label and selected path
+            LblBrowse.Text = "";
+
+
+        }
+
+        private void btnAppInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This app copies Valheim worlds and characters.\n" +
+                "To a destination of your choice.\nThis allows you to backup your precious creations.\n" +
+                "Love&Peace\n- S0ulL3sS");
+        }
+    }
+    
+}
